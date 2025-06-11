f90209a0bf203e38f404429564511b19eb637eb6
faizm10 <faizmustansar10@gmail.com>
2025-06-11 13:41:48 -0400

    switched to email+pw combo

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,31 +1,19 @@
 import { updateSession } from '@/lib/supabase/middleware'
-import { type NextRequest, NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  try {
-    return await updateSession(request);
-  } catch (error) {
-    console.error('Middleware error:', error);
-    return NextResponse.next(); // fallback to default if update fails
-  }
+  return await updateSession(request)
 }
 
-// import { updateSession } from '@/lib/supabase/middleware'
-// import { type NextRequest } from 'next/server'
-
-// export async function middleware(request: NextRequest) {
-//   return await updateSession(request)
-// }
-
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all request paths except for the ones starting with:
-//      * - _next/static (static files)
-//      * - _next/image (image optimization files)
-//      * - favicon.ico (favicon file)
-//      * Feel free to modify this pattern to include more paths.
-//      */
-//     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-//   ],
-// }
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
